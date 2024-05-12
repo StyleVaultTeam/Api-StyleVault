2251f7132a9d3256ec78f524216feb34a8a80881
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2024-05-12 15:50:07 -0500

    write cookies

---
+++ src/main/java/co/edu/escuelaing/cvds/lab7/config/CorsConfig.java
@@ -0,0 +1,39 @@
+package co.edu.escuelaing.cvds.lab7.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebMvc
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("rawtypes")
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(
+                Arrays.asList("http://localhost:3001", "https://mango-cliff-06b900910.5.azurestaticapps.net"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Credentials"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        config.setExposedHeaders(Arrays.asList("Set-Cookie"));
+        source.registerCorsConfiguration("/**", config);
+        @SuppressWarnings("unchecked")
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
